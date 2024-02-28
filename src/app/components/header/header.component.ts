6805222d713dcef2b6b70eb8ce6229dda1d8d5f2
Yorlin Quispe Ygnacio <yquispey@bcds.net>
2024-02-27 20:17:43 -0500

    mejor en vista de front

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  ngOnInit() {
+      this.loadScript();
+  }
 
+  loadScript() {
+    const body = document.body;
+    const script = document.createElement('script');
+    script.src = 'assets/js/main.js';
+    script.async = true;
+    script.defer = true;
+    body.appendChild(script);
+  }
+  
 }
